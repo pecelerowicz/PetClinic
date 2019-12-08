f24d3bb6444d26f01d7c3776a752ac3b72ce0e0f
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2019-12-08 13:11:04 +0100

    Refactor service interfaces to common crud interface

--- pet-clinic-data/src/main/java/com/mpecel/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/services/PetService.java
@@ -2,14 +2,6 @@ package com.mpecel.petclinic.services;
 
 import com.mpecel.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }

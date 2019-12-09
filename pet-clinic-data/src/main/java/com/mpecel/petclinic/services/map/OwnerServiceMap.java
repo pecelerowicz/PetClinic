7a3d10a4d0675ecee3470af7ef0f1c1a30d777c0
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2019-12-09 23:08:17 +0100

    Implement map-based services, closes #6

---
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package com.mpecel.petclinic.services.map;
+
+import com.mpecel.petclinic.model.Owner;
+import com.mpecel.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
---
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/services/map/PerServiceMap.java
@@ -0,0 +1,33 @@
+package com.mpecel.petclinic.services.map;
+
+import com.mpecel.petclinic.model.Pet;
+import com.mpecel.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PerServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return null;
+    }
+}

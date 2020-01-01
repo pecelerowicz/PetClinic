b8500a538b2608517cdad1f11e6897cd248d4036
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-01 14:00:49 +0100

    Add spring config

--- pet-clinic-data/src/main/java/com/mpecel/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.mpecel.petclinic.services.map;
 
 import com.mpecel.petclinic.model.Owner;
 import com.mpecel.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override

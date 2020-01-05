43b47c23273272bcaac6d302c460b031e3a2eeeb
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-06 00:17:15 +0100

    Add Specialities Map Service implementation. Close #31

---
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,35 @@
+package com.mpecel.petclinic.services.map;
+
+import com.mpecel.petclinic.model.Speciality;
+import com.mpecel.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}

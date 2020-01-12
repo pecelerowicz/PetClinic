718236623e9936333d9f656e4b19fbbae29bdcee
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-12 11:40:27 +0100

    Updated base entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/com/mpecel/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.mpecel.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass  // it says that we don't need this specific class mapped to the database (only classes deriving from it)
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // the database provides the identity value
     private Long id;
 
     public Long getId() {

39ea0fb7ed71209b752d15d25db2a72743954d95
Michal Pecelerowicz <michal.pecelerowicz@gmail.com>
2020-01-12 12:58:44 +0100

    Updated owner with JPA entity mappings. Closes #36

--- pet-clinic-data/src/main/java/com/mpecel/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.mpecel.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name") // by defalult there is a different naming strategy (?) this is redundant
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/mpecel/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/mpecel/petclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package com.mpecel.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id") // tells hibernate how to do the mapping; how the two entities are going to be related at the database level (we expect to have owner_id property in Pet records on the DB)
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
